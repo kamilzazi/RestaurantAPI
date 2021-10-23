346028241bdac24bcef2e536a4b11b139a535729
Kamil Zazula <zazi124@op.pl>
2021-10-23 11:12:13 +0200

    Add nlog - doesn't work

--- RestaurantAPI/Services/RestaurantService.cs
+++ RestaurantAPI/Services/RestaurantService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Models;
 using System;
@@ -22,11 +23,13 @@ namespace RestaurantAPI.Services
     {
         private readonly RestaurantDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILogger<RestaurantService> _logger;
 
-        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper, ILogger<RestaurantService> logger)
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _logger = logger;
         }
         public bool Update(int id, UpdateRestaurantDto dto)
         {
@@ -44,6 +47,7 @@ namespace RestaurantAPI.Services
 
         public bool Delete(int id)
         {
+            _logger.LogError($"Restaurant with id: {id} DELETE action invoked");
             var restaurant = _dbContext
                 .Restaurants
                 .FirstOrDefault(r => r.Id == id);
